9b60d9ed2789314f3db25152d4bf62cf1fd17449
gerd.kukemilk <gerd.kukemilk@tehik.ee>
2022-12-18 21:33:23 +0200

    1-4 Added with the initial commit.

--- homework4/task_5.py
+++ homework4/task_5.py
@@ -4,11 +4,20 @@ from random import randint
 class Bob:
 
     def __init__(self, verbose=False):
+        self.g = None
+        self.x = None
         self.h_1 = None
         self.h_2 = None
         self.y_1 = None
         self.y_2 = None
+        self.c = None
         self.verbose = verbose
+
+    def set_x_value(self, x):
+        self.x = x
+
+    def set_g_value(self, g):
+        self.g = g
 
     def listen(self, hashes):
         h_1, h_2 = hashes
@@ -18,17 +27,21 @@ class Bob:
         self.h_2 = h_2
 
     def send_c(self):
-        return randint(1, 10)
+        self.c = randint(1, 10)
+        return self.c
 
     def get_y_values(self, y_values):
         if self.verbose:
             print(f"Bob got y values: {y_values}")
         y_1, y_2 = y_values
-        self.h_1 = y_1
+        self.y_1 = y_1
         self.y_2 = y_2
 
     def verify(self):
         verified = False
+        if self.verbose:
+            print(f"Bob verifies with: "
+                  f"{self.y_1} * {self.y_2} = {self.h_1} * {self.h_2}")
         if self.y_1 and self.y_2:
             if self.y_1 * self.y_2 == self.h_1 * self.h_2:
                 verified = True
@@ -38,15 +51,21 @@ class Bob:
 class Alice:
     def __init__(self, verbose=False):
         self.c = None
-        self.g = randint(1, 10)
-        self.a = randint(1, 10)
-        self.b = randint(1, 10)
+        self.g = 17
+        self.a = 5
+        self.b = 7
         self.x = self.a + self.b
         self.r_1, self.r_2 = randint(1, 10), randint(1, 10)
         self.verbose = verbose
 
+    def get_g_value(self):
+        return self.g
+
+    def get_x_value(self):
+        return self.a + self.b
+
     def send_hashes(self):
-        return self.g**self.r_1, self.g**self.r_2
+        return self.g ** self.r_1, self.g ** self.r_2
 
     def get_c(self, c_value):
         if self.verbose:
@@ -58,25 +77,160 @@ class Alice:
             return self.c + self.a, self.c + self.b
 
 
-alice = Alice(verbose=True)
-bob = Bob(verbose=True)
-print(f"First alice sends hashes to Bob {alice.send_hashes()}")
-bob.listen(alice.send_hashes())
-print(f"Bob generate c and sends to Alice {bob.send_c()}")
-alice.get_c(bob.send_c())
-print(f"Alice sends bob y values: {alice.send_y_values()}")
-bob.get_y_values(alice.send_y_values())
-print(f"Bob verifies the communication: {bob.verify()}")
+class Eve(Alice):
+    def send_hashes(self):
+        return 1, 1
 
-count_of_verified = 0
-for i in range(0, 100):
-    alice = Alice()
-    bob = Bob()
+    def send_y_values(self):
+        return 1, 1
+
+    def set_x_value(self, x):
+        self.x = x
+
+
+class Evan(Bob):
+    def find_the_secrets(self):
+        if self.c and self.x:
+            "x = a + b"
+            ""
+            a = self.y_1 - self.c
+            b = self.y_2 - self.c
+            if self.verbose:
+                print(f"Evan found out a: {a} and b: {b}")
+            return a, b
+
+
+def completeness():
+    ## Legitimate communication.
+
+    alice = Alice(verbose=True)
+    bob = Bob(verbose=True)
+    bob.g = alice.g
+    bob.x = alice.x
+    print(f"Bob knows g and x = {bob.g, bob.x}")
+    print(f"First alice sends hashes to Bob {alice.send_hashes()}")
     bob.listen(alice.send_hashes())
+    print(f"Bob generate c and sends to Alice {bob.send_c()}")
     alice.get_c(bob.send_c())
+    print(f"Alice sends bob y values: {alice.send_y_values()}")
     bob.get_y_values(alice.send_y_values())
-    if bob.verify():
-        count_of_verified += 1
-        print(f"Verified times: {count_of_verified}")
-print(f"Bob has verified Alice {count_of_verified} times")
+    print(f"Bob verifies the communication with Alice: {bob.verify()}")
 
+    count_of_verified = 0
+    for i in range(0, 100):
+        alice = Alice()
+        bob = Bob()
+        bob.listen(alice.send_hashes())
+        alice.get_c(bob.send_c())
+        bob.get_y_values(alice.send_y_values())
+        if bob.verify():
+            count_of_verified += 1
+    return f"Bob has verified Alice {count_of_verified}/100 times"
+
+
+def soundness():
+    eve = Eve(verbose=True)
+    bob = Bob(verbose=True)
+    bob.g = eve.g
+    bob.x = eve.x
+    print(f"Bob knows g and x = {bob.g, bob.x}")
+    print(f"First Eve sends hashes to Bob {eve.send_hashes()}")
+    bob.listen(eve.send_hashes())
+    print(f"Bob generate c and sends to Eve {bob.send_c()}")
+    eve.get_c(bob.send_c())
+    print(f"Eve sends bob y values: {eve.send_y_values()}")
+    bob.get_y_values(eve.send_y_values())
+    print(f"Bob verifies the communication with Eve: {bob.verify()}")
+
+    count_of_verified = 0
+    for i in range(0, 100):
+        alice = Eve()
+        bob = Bob()
+        bob.listen(eve.send_hashes())
+        alice.get_c(bob.send_c())
+        bob.get_y_values(eve.send_y_values())
+        if bob.verify():
+            count_of_verified += 1
+    return f"Bob has verified Eve {count_of_verified}/100 times."
+
+
+def zero_knowledge():
+    alice = Alice()
+    evan = Evan()
+    evan.g = alice.g
+    evan.x = alice.x
+    print(f"Only Alice knows a = {alice.a} and b = {alice.b}")
+    print(f"Evan knows g = {evan.g} and x = {evan.x}")
+    print(f"First Alice sends hashes to Evan {alice.send_hashes()}")
+    evan.listen(alice.send_hashes())
+    print(f"Evan generates c and sends to Alice {evan.send_c()}")
+    alice.get_c(evan.send_c())
+    print(f"Alice sends Evan y values: {alice.send_y_values()}")
+    evan.get_y_values(alice.send_y_values())
+    evan.find_the_secrets()
+    print(f"Evan verifies the communication with Alice: {evan.verify()}")
+    a, b = evan.find_the_secrets()
+    return f"Evan found Alice's a = {a} and b = {b} values that should be secret."
+
+
+print(f"-------------")
+print(f"Completeness:")
+print(f"If the claim is true, Bob always accepts it.")
+print(f"-------------")
+print(f"""
+    For this protocol to pass the completeness check
+    Alice should be able to prove to Bob that she is Alice
+    given that she has followed the protocol correctly.
+    In this example Alice is never able to prove to Bob that she is Alice even though
+    she has followed the protocol.
+    This is because Bob will check that Alice is who she said she is 
+    by the equation of y_1 * y_2 = h_1 * h_2. But the values of y are exponents
+    of g^r and the values for h are additions of c = rand() and Alice's private keys.
+    Since Alice can never know the c value that bob sends,
+    she can never prepare her answer to pass the validation.
+    Completeness means that
+    Alice should be able to prove herself reliably, which she can't.
+    
+    Example: {completeness()}
+""")
+
+print(f"-------------")
+print(f"Soundness:")
+print(f"Eve cannot convince Bob that statement is true if it is actually false.")
+print(f"-------------")
+print(f"""
+    The principle of soundness means that Eve's false claim should be only
+    accepted with a very small probability.
+    In this example the protocol is clearly not sound,
+    because Bob will never accept Legitimate claims by Alice, but will always
+    accept Eve's false claims.
+    Example: Eve chooses y and h values such that the verification step will pass.
+    {soundness()}
+""")
+
+print(f"-------------")
+print(f"Zero-knowledge::")
+print(f"Bob only learns the truth value of the claim and nothing else.")
+print(f"-------------")
+print(f"""
+    As taken from the lecture notes of Ahto Puldas:
+    There is an efficient simulator M that is able to generate
+    the transcript of the protocol (with the same probability distribution as in
+    the protocol) knowing only the truth value of the claim.
+    Hence, Bob only learns the truth value of the claim, because if he knows
+    the truth value, he can simulate the communication himself.
+    
+    In other words, the prover has to be able to prove that she knows a secret without
+    revealing the secret and the verifier has to be able to verify that the prover indeed knows
+    the secret without actually being able to see the secret.
+    
+    So Alice knows a and b values and Bob only knows x value that is composed of the a and b value.
+    Bob should not be able to find out the values of a and b to satisfy Zero-knowledge.
+    
+    Example: Alice starts the protocol with malicious Evan who she thinks is Bob.
+    Alice uses her secrets thinking that Evan can't find them out.
+    Evan follows the protocol until getting Alices y values and then substracts the c value
+    that he sent to Alice to find Alice's secrets.
+    But actually:
+    {zero_knowledge()}
+""")
